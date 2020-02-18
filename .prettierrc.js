02129a7db4e2235615d37ecb95e59c25854d9692
Ross Gibson <rossgibson@me.com>
2020-02-18 15:27:53 +0000

    Configure dev dependencies

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-  bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  // Use spaces for tabs with a tab width of 2 spaces
+  tabWidth: 2,
+  useTabs: false,
+  // Good: { foo: bar }
+  // Bad: {foo: bar}
+  bracketSpacing: true,
+  // Good: x => y
+  // Bad: (x) => y
+  arrowParens: 'avoid',
 };
