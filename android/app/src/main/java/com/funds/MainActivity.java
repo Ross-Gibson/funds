c1571abdbe6fe0ba7c09790a2dc05563387e81ba
Ross Gibson <rossgibson@me.com>
2020-02-19 20:05:16 +0000

    Resolves Android build issues

--- android/app/src/main/java/com/funds/MainActivity.java
+++ android/app/src/main/java/com/funds/MainActivity.java
@@ -1,15 +1,30 @@
 package com.funds;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
     return "funds";
+  }
+
+  // Required for react-native-gesture-handler:
+  // https://reactnavigation.org/docs/en/next/stack-navigator.html
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
