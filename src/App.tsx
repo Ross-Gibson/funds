d15ec8d243191589e6975dc7de013c74af9921a5
Ross Gibson <rossgibson@me.com>
2020-02-19 14:49:57 +0000

    Adds additional expense info to the UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,58 +9,11 @@
 import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   FlatList,
   ActivityIndicator,
   Text,
+  View,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 function App() {
   const [expenses, setExpenses] = useState([]);
@@ -95,7 +48,14 @@ function App() {
     <SafeAreaView>
       <FlatList
         data={expenses}
-        renderItem={({ item }) => <Text>{item.user.email}</Text>}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.date}</Text>
+            <Text>{item.merchant}</Text>
+            <Text>{item.amount.value}</Text>
+            <Text>{item.user.email}</Text>
+          </View>
+        )}
         keyExtractor={item => item.id}
       />
     </SafeAreaView>
