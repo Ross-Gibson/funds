ff747e5c72b89d4a968c642a5975883e140e03f7
Ross Gibson <rossgibson@me.com>
2020-02-20 16:17:46 +0000

    Adds basic styles to the expenses screen

--- src/screens/Expenses.tsx
+++ src/screens/Expenses.tsx
@@ -1,11 +1,30 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView, FlatList, ActivityIndicator, View } from 'react-native';
+import { FlatList, ActivityIndicator, View, StyleSheet } from 'react-native';
 import { NavigationParams } from 'react-navigation';
-import { Theme, withTheme, Text } from 'react-native-paper';
+import {
+  Theme,
+  withTheme,
+  Text,
+  List,
+  Avatar,
+  Divider,
+} from 'react-native-paper';
 import { connect, ConnectedProps } from 'react-redux';
+import moment from 'moment';
 
 import { RootState } from '../store/types';
 import { fetchExpenses as fetchExpensesAction } from '../store/expenses/actions';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  listItem: {
+    marginVertical: 0,
+    height: 80,
+    paddingHorizontal: 16,
+  },
+});
 
 const mapState = (state: RootState) => ({
   loading: state.expenses.loading,
@@ -32,6 +51,8 @@ function Expenses({
   expenses,
   fetchExpenses,
 }: Props) {
+  const { colors } = theme;
+
   useEffect(() => {
     fetchExpenses({ limit: 25, offset: 0 });
   }, []);
@@ -46,21 +67,34 @@ function Expenses({
   }
 
   return (
-    <SafeAreaView>
-      <FlatList
-        data={expenses}
-        extraData={expenses}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.date}</Text>
-            <Text>{item.merchant}</Text>
-            <Text>{item.amount.value}</Text>
-            <Text>{item.user.email}</Text>
-          </View>
-        )}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
+    <FlatList
+      style={[styles.container, { backgroundColor: colors.background }]}
+      data={expenses}
+      extraData={expenses}
+      renderItem={({ item }) => (
+        <View>
+          <List.Subheader>
+            {moment(item.date).format('D MMM YYYY')}
+          </List.Subheader>
+          <List.Item
+            style={styles.listItem}
+            title={item.merchant}
+            description={moment(item.date).format('h:mm a')}
+            right={props => (
+              <Text>{`${item.amount.value} ${item.amount.currency}`}</Text>
+            )}
+            left={props => (
+              <Avatar.Image
+                size={48}
+                source={{ uri: 'https://i.pravatar.cc/48' }}
+              />
+            )}
+          />
+          <Divider inset={true} />
+        </View>
+      )}
+      keyExtractor={item => item.id}
+    />
   );
 }
 
