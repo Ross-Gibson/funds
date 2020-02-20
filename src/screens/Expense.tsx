4ccde181f80fcaafbd0b5b435bf3fd28b2f89cbf
Ross Gibson <rossgibson@me.com>
2020-02-20 20:21:08 +0000

    Add a receipt placeholder to the expense detail view

--- src/screens/Expense.tsx
+++ src/screens/Expense.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Dimensions } from 'react-native';
 import { NavigationParams } from 'react-navigation';
 import {
   Theme,
@@ -16,14 +16,34 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { ExpensesStackParamList } from '../navigation/Stack';
 import { useLocalization } from '../contexts/localization';
 
+const goldenRatio = 1.62;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  contentContainer: {
+    paddingHorizontal: 16,
   },
   listItem: {
     marginVertical: 0,
     height: 80,
     paddingHorizontal: 16,
+  },
+  surface: {
+    flex: 1,
+    padding: 16,
+    width: '100%',
+    height: (Dimensions.get('window').width - 2 * 16) / goldenRatio,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 0,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  addReceiptLabel: {
+    paddingVertical: 8,
   },
 });
 
@@ -83,6 +103,23 @@ function Expense({ navigation, theme, route }: Props) {
           />
         )}
       />
+      <View style={styles.contentContainer}>
+        <View
+          style={[
+            styles.surface,
+            {
+              borderColor: colors.disabled,
+              backgroundColor: colors.disabled,
+            },
+          ]}>
+          <Avatar.Icon
+            size={64}
+            icon={() => <Icon name={'plus'} size={24} color={colors.surface} />}
+            color={colors.surface}
+          />
+          <Text style={styles.addReceiptLabel}>Add receipt</Text>
+        </View>
+      </View>
     </ScrollView>
   );
 }
